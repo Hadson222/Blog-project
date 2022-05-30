09c1bfc3add5c6646809d39856652b05c9fab7c7
hadson222 <hadinhson2000@gmail.com>
2022-05-31 00:05:52 +0700

    Make create

---
+++ resources/views/create.blade.php
@@ -0,0 +1,12 @@
+<form action="form.php" method="POST">
+    <div>
+        <label for="Title">Title</label>
+        <input type="text" id="title" name="title" />
+    <div>
+        <label for="content">Content:</label>
+        <input type="text" id="content" name="content" />
+    </div>
+
+    </div>
+        <button type="submit">Submit</button>
+</form>
